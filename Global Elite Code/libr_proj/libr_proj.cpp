1a27e5b7bde3df0a114041a8a3eec14861c83f42
YLStoyanov19 <YLStoyanov19@codingurgas.bg>
2022-05-08 23:52:23 +0300

    Fix Front-End errors

--- Global Elite Code/libr_proj/libr_proj.cpp
+++ Global Elite Code/libr_proj/libr_proj.cpp
@@ -7,14 +7,18 @@
 using namespace std;
 int h = 0;
 void mainMenu() {
-    
+    HANDLE l = GetStdHandle(STD_OUTPUT_HANDLE);
+
     int gender = 0;
+    SetConsoleTextAttribute(l, 13);
 
     cout << endl <<  "             Select your gender  " << endl;
 
-    cout <<          "          Female - 1       Male - 2" << endl << endl;
+    cout <<          "          Female - 1 " << SetConsoleTextAttribute(l, 2) << "      Male - 2" << endl << endl;
 
     cin >> gender;
+
+    SetConsoleTextAttribute(l, 10);
 
     cout << "Please enter your height:" << endl << endl;
 
@@ -23,11 +27,15 @@ void mainMenu() {
 
 
 void bmiCalc() {
+    HANDLE l = GetStdHandle(STD_OUTPUT_HANDLE);
+    
     int gender= 0, height =0 , kg = 0;
     if ((h < 50 && gender == 1) || (h < 58 && gender == 2))
     {
+        SetConsoleTextAttribute(l, 4);
         cout << "Incorrect input";
     }
+    
     else
     {
         if (gender == 1)
@@ -39,7 +47,8 @@ void bmiCalc() {
                 kg++;
             }
             if (height > 175)kg += 3.5;
-            cout << kg;
+            SetConsoleTextAttribute(l, 6);
+            cout << "Your perfect body mass is: " << kg << endl << endl;
         }
         else
         {
@@ -51,13 +60,17 @@ void bmiCalc() {
                 
             }
             if (height > 175)kg += 5.5;
-            cout << kg;
+            SetConsoleTextAttribute(l, 6);
+            cout << "Your perfect body mass is: " << kg << endl << endl;
         }
     }
 }
 
 
 void foodCalc() {
+
+    HANDLE l = GetStdHandle(STD_OUTPUT_HANDLE);
+
     int a, b;
 
     cout << endl;
@@ -66,30 +79,63 @@ void foodCalc() {
 
     cout << endl;
 
+    SetConsoleTextAttribute(l, 13);
+
     cout << " 1 - Meatballs     " << "     11 - Carrot" << "             21 - Spinach" << endl;
+
+    SetConsoleTextAttribute(l, 6);
+
     cout << " 2 - Mashed potato " << "     12 - Red cabbage " <<"       22 - Lemon" << endl;
+
+    SetConsoleTextAttribute(l, 13);
+
     cout << " 3 - Meatloaf" << "           13 - Cabbage" << "            23 - Mango" << endl;
+
+    SetConsoleTextAttribute(l, 6);
+
     cout << " 4 - Mushroom " << "          14 - Orange " << "            24 - Watermelon" << endl;
+
+    SetConsoleTextAttribute(l, 13);
+
     cout << " 5 - Cheese " << "            15 - Bacon" << "              25 - Strawberry" << endl;
+
+    SetConsoleTextAttribute(l, 6);
+
     cout << " 6 - Mozzarella " << "        16 - Ham" << "                26 - Kiwi" << endl;
+
+    SetConsoleTextAttribute(l, 13);
+
     cout << " 7 - Parmesan " << "          17 - Pepperoni " << "         27 - Pomelo" << endl;
+
+    SetConsoleTextAttribute(l, 6);
+
     cout << " 8 - Cheddar cheese" << "     18 - Sausage" << "            28 - Blueberry" << endl;
+
+    SetConsoleTextAttribute(l, 13);
+
     cout << " 9 - Meat " << "              19 - Onion " << "             29 - Chicken" << endl;
+
+    SetConsoleTextAttribute(l, 6);
+
     cout << "10 - Tomato " << "            20 - Brussels sprout" << "    30 - Rice" << endl << endl;
 
 
 
     cin >> a;
 
+    cout << endl;
 
 
-    cout << "How many grams" << endl;
+    SetConsoleTextAttribute(l, 10);
 
+    cout << "How many grams: " ;
 
 
     cin >> b;
+    
+    cout << endl;
 
-
+    SetConsoleTextAttribute(l, 10);
 
     cout << "Your kcal - ";
 
@@ -99,36 +145,64 @@ void foodCalc() {
     {
 
 
-
     case 1: cout << b * 1.97; break;
+
     case 2: cout << b * 0.88; break;
+
     case 3: cout << b * 1.49; break;
+
     case 4: cout << b * 0.22; break;
+
     case 5: cout << b * 4.2; break;
+
     case 6: cout << b * 2.80; break;
+
     case 7: cout << b * 4.31; break;
+
     case 8: cout << b * 4.2; break;
+
     case 9: cout << b * 1.43; break;
+
     case 10: cout << b * 0.22; break;
+
     case 11:cout << b * 0.11; break;
+
     case 12: cout << b * 0.31; break;
+
     case 13: cout << b * 0.25; break;
+
     case 14: cout << b * 0.47; break;
+
     case 15: cout << b * 5.41; break;
+
     case 16: cout << b * 1.45; break;
+
     case 17: cout << b * 4.94; break;
+
     case 18: cout << b * 3.46; break;
+
     case 19: cout << b * 0.4; break;
+
     case 20: cout << b * 0.43; break;
+
     case 21: cout << b * 0.23; break;
+
     case 22: cout << b * 0.29; break;
+
     case 23: cout << b * 0.6; break;
+
     case 24: cout << b * 0.3; break;
+
     case 25: cout << b * 0.33; break;
+
     case 26: cout << b * 0.61; break;
+
     case 27: cout << b * 0.38; break;
+
     case 28: cout << b * 0.3; break;
+
     case 29: cout << b * 2.39; break;
+
     case 30: cout << b * 1.3; break;
 
     }
